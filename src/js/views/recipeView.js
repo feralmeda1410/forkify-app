40b4e52ec2d43b0abdab5353b39108987d43901d
IoriFernando <feralmeda@hotmail.com>
2026-09-11 14:00:52 -0600

    Avance 4 completado: vistas y paginacion

---
+++ src/js/views/recipeView.js
@@ -0,0 +1,52 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+// import { Fraction } from 'fractional';
+
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errorMessage = 'We could not find that recipe. Please try another one!';
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
+  _generateMarkup() {
+    return `
+      <figure class="recipe__fig">
+        <img src="${this._data.image}" alt="${this._data.title}" class="recipe__img" />
+        <h1 class="recipe__title"><span>${this._data.title}</span></h1>
+      </figure>
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon"><use href="${icons}#icon-clock"></use></svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookTime}</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon"><use href="${icons}#icon-users"></use></svg>
+          <span class="recipe__info-data recipe__info-data--people">${this._data.servings}</span>
+          <span class="recipe__info-text">servings</span>
+        </div>
+      </div>
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
+        </ul>
+      </div>
+    `;
+  }
+
+  _generateMarkupIngredient(ing) {
+    return `
+      <li class="recipe__ingredient">
+        <svg class="recipe__icon"><use href="${icons}#icon-check"></use></svg>
+<div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>        <div class="recipe__description">
+          <span class="recipe__unit">${ing.unit}</span>
+          ${ing.description}
+        </div>
+      </li>
+    `;
+  }
+}
+export default new RecipeView();
