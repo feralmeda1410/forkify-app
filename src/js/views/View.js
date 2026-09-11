eff4b52143c0672217a94c85f49b7a387cbd1b8f
IoriFernando <feralmeda@hotmail.com>
2026-09-11 15:01:11 -0600

    Solucion definitiva para iconos en Netlify

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,4 +1,4 @@
-import icons from 'url:../../img/icons.svg';
+import icons from '../../img/icons.svg';
 
 export default class View {
   _data;
@@ -18,7 +18,9 @@ export default class View {
   renderSpinner() {
     const markup = `
       <div class="spinner">
-        <svg><use href="${icons}#icon-loader"></use></svg>
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
       </div>
     `;
     this._clear();
@@ -28,7 +30,26 @@ export default class View {
   renderError(message = this._errorMessage) {
     const markup = `
       <div class="error">
-        <div><svg><use href="${icons}#icon-alert-triangle"></use></svg></div>
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
         <p>${message}</p>
       </div>
     `;
